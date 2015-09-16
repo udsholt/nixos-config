95539795165d07eb81c4aa3a79e800d2749591c5
Jakob Udsholt <jakob@udsholt.dk>
2015-09-16 19:00:50 +0200

    use inputClassSections for mouse setup

--- kaaresyv.nix
+++ kaaresyv.nix
@@ -9,7 +9,6 @@
         ./common/i3.nix
         ./common/golang.nix
         ./common/networkmanager.nix
-        ./common/vagrant.nix
         ./common/steam.nix
         ./common/plex.nix
         ./common/dnsmasq.nix
--- system/kaaresyv.nix
+++ system/kaaresyv.nix
@@ -4,26 +4,26 @@
   # Hostname definition
   networking.hostName = "kaaresyv";
   networking.hostId = "829af73f";
-  
+
   # Bootloader
   boot.loader.gummiboot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   # Use refind for dual booting
   #
-  # Install with: 
+  # Install with:
   #   > sudo refind-install
-  # 
+  #
   # From: http://www.rodsbooks.com/refind/installing.html
   #
-  # In case of bus errors this might help 
+  # In case of bus errors this might help
   #   > sudo efibootmgr -c -l \\EFI\\refind\\refind_x64.efi -L rEFInd
   #
   environment.systemPackages = with pkgs; [
     refind
     efibootmgr
   ];
-  
+
   # Timezone
   time.timeZone = "Europe/Copenhagen";
 
@@ -55,34 +55,72 @@
   # Figure out if i need swap later
   swapDevices = [ ];
 
-  # Use nvidia binary driver
-  # NOTE: this is required for i3bar to work...
+  # Setup xserver
   services.xserver = {
+
+      # Use nvidia binary driver
+      # NOTE: this is required for i3bar to work!
       videoDrivers = [ "nvidia" ];
 
-      # Not yet supported
-      /*
+      # Setup mouse.
+      #
+      # xinput dump of defaults
+      #
+      # Device 'Razer Razer DeathAdder 2013':
+      #     Device Enabled (150):   1
+      #     Coordinate Transformation Matrix (152): 1.000000, 0.000000, 0.000000, 0.000000, 1.000000, 0.000000, 0.000000, 0.000000, 1.000000
+      #     Device Accel Profile (275): 0
+      #     Device Accel Constant Deceleration (276):   1.000000
+      #     Device Accel Adaptive Deceleration (277):   1.000000
+      #     Device Accel Velocity Scaling (278):    10.000000
+      #     Device Product ID (268):    5426, 55
+      #     Device Node (269):  "/dev/input/event6"
+      #     Evdev Axis Inversion (279): 0, 0
+      #     Evdev Axes Swap (281):  0
+      #     Axis Labels (282):  "Rel X" (160), "Rel Y" (161), "Rel Vert Wheel" (274)
+      #     Button Labels (283):    "Button Left" (153), "Button Middle" (154), "Button Right" (155), "Button Wheel Up" (156), "Button Wheel Down" (157), "Button Horiz Wheel Left" (158), "Button Horiz Wheel Right" (159), "Button Side" (272), "Button Extra" (273), "Button Unknown" (271), "Button Unknown" (271), "Button Unknown" (271), "Button Unknown" (271)
+      #     Evdev Scrolling Distance (284): 1, 1, 1
+      #     Evdev Middle Button Emulation (285):    0
+      #     Evdev Middle Button Timeout (286):  50
+      #     Evdev Third Button Emulation (287): 0
+      #     Evdev Third Button Emulation Timeout (288): 1000
+      #     Evdev Third Button Emulation Button (289):  3
+      #     Evdev Third Button Emulation Threshold (290):   20
+      #     Evdev Wheel Emulation (291):    0
+      #     Evdev Wheel Emulation Axes (292):   0, 0, 4, 5
+      #     Evdev Wheel Emulation Inertia (293):    10
+      #     Evdev Wheel Emulation Timeout (294):    200
+      #     Evdev Wheel Emulation Button (295): 4
+      #     Evdev Drag Lock Buttons (296):  0
+      #
+      # Old mouse setup:
+      #
+      #     xinput --set-prop 10 "Device Accel Constant Deceleration" 1.65
+      #     xinput --set-prop 10 "Device Accel Adaptive Deceleration" 1
+      #     xinput --set-prop 10 "Device Accel Velocity Scaling" 1.1
+      #
       inputClassSections = [''
           Identifier "Razer DeathAdder 2013 Slowdown"
 
           MatchIsPointer "true"
           MatchProduct   "Razer Razer DeathAdder 2013"
 
-          Option "Device Accel Constant Deceleration" "1.6"
-          Option "Device Accel Adaptive Deceleration" "1.0"
-          Option "Device Accel Velocity Scaling"      "1.1"
+          Option "Device Accel Constant Deceleration" "1.65"
+          Option "Device Accel Adaptive Deceleration" "1.00"
+          Option "Device Accel Velocity Scaling"      "1.10"
       ''];
-      */
   };
-  
+
+
+
   # Has something to do with loading hardware
   imports = [ <nixpkgs/nixos/modules/installer/scan/not-detected.nix> ];
-  
+
   # Detected by nixos install
   boot.initrd.availableKernelModules = [ "xhci_hcd" "ehci_pci" "ahci" "usbhid" ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
-  
+
   # 8 cores
   nix.maxJobs = 8;
 }
