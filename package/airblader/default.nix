54ef8c52d464d9e9d5755a7f65ec0ffee7f30838
Jakob Udsholt <jakob@udsholt.dk>
2015-11-05 10:34:18 +0100

    move away from fetch git (non deterministic) for airblader

--- package/airblader/default.nix
+++ package/airblader/default.nix
@@ -1,17 +1,14 @@
-{ fetchgit, stdenv, which, pkgconfig, makeWrapper, libxcb, xcbutilkeysyms
+{ fetchgit, fetchurl, stdenv, which, pkgconfig, makeWrapper, libxcb, xcbutilkeysyms
 , xcbutil, xcbutilwm, libstartup_notification, libX11, pcre, libev, yajl
-, xcb-util-cursor, coreutils, perl, pango, perlPackages, xdummy, libxkbcommon, git }:
+, xcb-util-cursor, coreutils, perl, pango, perlPackages, xdummy, libxkbcommon }:
 
 stdenv.mkDerivation rec {
   name = "airblader-${version}";
   version = "4.11";
 
-  src = fetchgit {
-    name = "i3-airblader";
-    url = https://github.com/Airblader/i3.git;
-    rev = "6ea58a840c4f71c398e1d81e91c423c6cbcaa84b";
-    sha256 = "1dj3q9615hkf3d0h804jmk6ddsjlz1znfjl85lzmdxidjv65a0h9";
-    leaveDotGit = true;
+  src = fetchurl {
+    url = "https://github.com/Airblader/i3/archive/${version}.tar.gz";
+    sha256 = "1kxhcsggj115y2xnw4hxcfzq2ghis2jjwgh9w2jny760dzc89h8f";
   };
 
   buildInputs = [
@@ -19,7 +16,7 @@ stdenv.mkDerivation rec {
     libstartup_notification libX11 pcre libev yajl xcb-util-cursor perl pango
     perlPackages.AnyEventI3 perlPackages.X11XCB perlPackages.IPCRun
     perlPackages.ExtUtilsPkgConfig perlPackages.TestMore perlPackages.InlineC
-    libxkbcommon git
+    libxkbcommon
   ];
 
   postPatch = ''
@@ -27,15 +24,15 @@ stdenv.mkDerivation rec {
   '';
 
   # Fix for git decribe:
-  #
-  #   leaveDotGit = true
-  #   git tag ${version}
-  #
   # * https://github.com/Airblader/i3/issues/43
   # * https://github.com/Airblader/i3/issues/26
+  postUnpack = ''
+    echo "${version} ($(date +"%Y-%m-%d"), branch \\\"${version}\\\")" > $sourceRoot/I3_VERSION
+    echo "${version}" > $sourceRoot/VERSION
+  '';
+
   configurePhase = ''
     makeFlags=PREFIX=$out;
-    git tag ${version}
   '';
 
   postInstall = ''
