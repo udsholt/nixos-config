bfd9d8c08709ff53d5226ee926a8c35c19402ce7
Jakob Udsholt <jakob@udsholt.dk>
2017-12-11 22:57:05 +0100

    add container

---
+++ container/stats.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+    containers.stats = {
+        config = { config, pkgs, ... }: {
+
+            services.influxdb = {
+                enable = true;
+            };
+
+            services.grafana = {
+                enable = true;
+            };
+            
+        };
+    };
+}
