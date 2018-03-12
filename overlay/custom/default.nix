e8ad481d089619008b970275d26fb16447530e7f
Jakob Udsholt <jakob@udsholt.dk>
2018-03-12 09:20:06 +0100

    nothing

--- overlay/custom/default.nix
+++ overlay/custom/default.nix
@@ -1,5 +1,8 @@
 self: super:
 
+# http://nbp.github.io/slides/NixCon/2017.NixpkgsOverlays/
+# https://blog.flyingcircus.io/2017/11/07/nixos-the-dos-and-donts-of-nixpkgs-overlays/
+
 {
     font-awesome = super.callPackage ./package/font-awesome {};
 
