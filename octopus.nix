43182cc9d286e640cd9f5dc6d25fa82fe67ec020
Jakob Udsholt <jakob@udsholt.dk>
2018-11-01 12:23:40 +0100

    add octopus

---
+++ octopus.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+    require = [
+        ./system/octopus.nix
+
+        ./common/user.nix
+        ./common/base.nix
+        ./common/desktop.nix
+        ./common/i3.nix
+        ./common/sybo.nix
+        ./common/node.nix
+    ];
+}
