f39f04bde6e40b37c89eb90e8747ea5570e4ef0a
Jakob Udsholt <jakob@sybogames.com>
2019-05-02 13:54:35 +0200

    update octopus to 19

--- system/octopus.nix
+++ system/octopus.nix
@@ -1,32 +1,30 @@
 { config, lib, pkgs, ... }:
 
 {
-    system.stateVersion = "18.09";
+    system.stateVersion = "19.03";
 
-    services.vmwareGuest.enable = true;
+    virtualisation.vmware.guest.enable = true;
 
     networking.hostName = "octopus";
 
     time.timeZone = "Europe/Copenhagen";
 
-	boot.loader.grub.enable = true;
-	boot.loader.grub.version = 2;
-	boot.loader.grub.device = "/dev/sda";
+	  boot.loader.grub.enable = true;
+	  boot.loader.grub.version = 2;
+	  boot.loader.grub.device = "/dev/sda";
 
     boot.initrd.availableKernelModules = [ "ata_piix" "mptspi" "uhci_hcd" "ehci_pci" "sd_mod" "sr_mod" ];
     boot.kernelModules = [ "kvm-intel" ];
     boot.extraModulePackages = [ ];
 
     fileSystems."/" = {
- 		device = "/dev/disk/by-uuid/429c9801-dfb4-45ea-9204-d0d3dd4ae56b";
-		fsType = "ext4";
+        device = "/dev/disk/by-uuid/429c9801-dfb4-45ea-9204-d0d3dd4ae56b";
+        fsType = "ext4";
     };
 
   	swapDevices = [
-    	{ device = "/dev/disk/by-uuid/cc1fc00b-89ba-4689-a407-db6ccc70dc3d"; }
+    	  { device = "/dev/disk/by-uuid/cc1fc00b-89ba-4689-a407-db6ccc70dc3d"; }
     ];
 
     nix.maxJobs = lib.mkDefault 4;
 }
-
-
