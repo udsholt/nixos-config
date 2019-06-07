58df3a1925a5287f19cd6673f575f22ddfd55a4a
Jakob Udsholt <jakob@sybogames.com>
2019-06-07 17:07:14 +0200

    vscode 1.35

--- overlay/custom/package/vscode/default.nix
+++ overlay/custom/package/vscode/default.nix
@@ -1,14 +1,6 @@
-{ stdenv, lib, fetchurl, makeDesktopItem
-, unzip, libsecret, libXScrnSaver, wrapGAppsHook
-, gtk2, atomEnv, at-spi2-atk, autoPatchelfHook
-, systemd, fontconfig
-, isInsiders ? false }:
+{ stdenv, lib, callPackage, fetchurl, fetchpatch, isInsiders ? false }:
 
 let
-  executableName = "code" + lib.optionalString isInsiders "-insiders";
-  longName = "Visual Studio Code" + lib.optionalString isInsiders " - Insiders";
-  shortName = "Code" + lib.optionalString isInsiders " - Insiders";
-
   inherit (stdenv.hostPlatform) system;
 
   plat = {
@@ -17,17 +9,22 @@ let
     "x86_64-darwin" = "darwin";
   }.${system};
 
-  sha256 = {
-    "i686-linux" = "0n2k134yx0zirddi5xig4zihn73s8xiga11pwk90f01wp1i0hygg";
-    "x86_64-linux" = "0ljijcqfyrfck5imldis3hx9d9iacnspgnm58kvlziam8y0imwzv";
-    "x86_64-darwin" = "00fg106rggsbng90k1jjp1c6nmnwni5s0fgmbz6k45shfa3iqamc";
-  }.${system};
-
   archive_fmt = if system == "x86_64-darwin" then "zip" else "tar.gz";
+
+  sha256 = {
+    "i686-linux" = "1485maq7rrvi742w0zs5nnaqy2s7w4hhm0fi4n69vafncia8zyic";
+    "x86_64-linux" = "082725c7yzih13d4khvwz34ijwdg6yxmsxhjmpn2pqlfsg43hxsh";
+    "x86_64-darwin" = "1mvj63sbdcw227bi4idqcwqxds60g64spvdi2bxh5sk6g5q5df90";
+  }.${system};
 in
-  stdenv.mkDerivation rec {
-    name = "vscode-${version}";
-    version = "1.33.1";
+  callPackage ./generic.nix rec {
+
+    version = "1.35.0";
+    pname = "vscode";
+
+    executableName = "code" + lib.optionalString isInsiders "-insiders";
+    longName = "Visual Studio Code" + lib.optionalString isInsiders " - Insiders";
+    shortName = "Code" + lib.optionalString isInsiders " - Insiders";
 
     src = fetchurl {
       name = "VSCode_${version}_${plat}.${archive_fmt}";
@@ -35,84 +32,7 @@ in
       inherit sha256;
     };
 
-    passthru = {
-      inherit executableName;
-    };
-
-    desktopItem = makeDesktopItem {
-      name = executableName;
-      desktopName = longName;
-      comment = "Code Editing. Redefined.";
-      genericName = "Text Editor";
-      exec = executableName;
-      icon = "@out@/share/pixmaps/code.png";
-      startupNotify = "true";
-      categories = "Utility;TextEditor;Development;IDE;";
-      mimeType = "text/plain;inode/directory;";
-      extraEntries = ''
-        StartupWMClass=${shortName}
-        Actions=new-empty-window;
-        Keywords=vscode;
-
-        [Desktop Action new-empty-window]
-        Name=New Empty Window
-        Exec=${executableName} --new-window %F
-        Icon=@out@/share/pixmaps/code.png
-      '';
-    };
-
-    urlHandlerDesktopItem = makeDesktopItem {
-      name = executableName + "-url-handler";
-      desktopName = longName + " - URL Handler";
-      comment = "Code Editing. Redefined.";
-      genericName = "Text Editor";
-      exec = executableName + " --open-url %U";
-      icon = "@out@/share/pixmaps/code.png";
-      startupNotify = "true";
-      categories = "Utility;TextEditor;Development;IDE;";
-      mimeType = "x-scheme-handler/vscode;";
-      extraEntries = ''
-        NoDisplay=true
-        Keywords=vscode;
-      '';
-    };
-
-    buildInputs = (if stdenv.isDarwin
-      then [ unzip ]
-      else [ gtk2 at-spi2-atk wrapGAppsHook ] ++ atomEnv.packages)
-        ++ [ libsecret libXScrnSaver ];
-
-    nativeBuildInputs = lib.optional (!stdenv.isDarwin) autoPatchelfHook;
-
-    dontBuild = true;
-    dontConfigure = true;
-
-    installPhase =
-      if system == "x86_64-darwin" then ''
-        mkdir -p $out/lib/vscode $out/bin
-        cp -r ./* $out/lib/vscode
-        ln -s $out/lib/vscode/Contents/Resources/app/bin/${executableName} $out/bin
-      '' else ''
-        mkdir -p $out/lib/vscode $out/bin
-        cp -r ./* $out/lib/vscode
-
-        substituteInPlace $out/lib/vscode/bin/${executableName} --replace '"$CLI" "$@"' '"$CLI" "--skip-getting-started" "$@"'
-
-        ln -s $out/lib/vscode/bin/${executableName} $out/bin
-
-        mkdir -p $out/share/applications
-        substitute $desktopItem/share/applications/${executableName}.desktop $out/share/applications/${executableName}.desktop \
-          --subst-var out
-        substitute $urlHandlerDesktopItem/share/applications/${executableName}-url-handler.desktop $out/share/applications/${executableName}-url-handler.desktop \
-          --subst-var out
-
-        mkdir -p $out/share/pixmaps
-        cp $out/lib/vscode/resources/app/resources/linux/code.png $out/share/pixmaps/code.png
-      '';
-
-    preFixup = lib.optionalString (system == "i686-linux" || system == "x86_64-linux") ''
-      gappsWrapperArgs+=(--prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ systemd fontconfig ]})
-    '';
+    sourceRoot = "";
 
     meta = with stdenv.lib; {
       description = ''
@@ -129,7 +49,7 @@ in
       homepage = https://code.visualstudio.com/;
       downloadPage = https://code.visualstudio.com/Updates;
       license = licenses.unfree;
-      maintainers = with maintainers; [ eadwu ];
+      maintainers = with maintainers; [ eadwu synthetica ];
       platforms = [ "i686-linux" "x86_64-linux" "x86_64-darwin" ];
     };
   }
