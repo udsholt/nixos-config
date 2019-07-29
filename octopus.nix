140c5365cdb54c7243c3e12f64b266c3cc1d079d
Jakob Udsholt <jakob@sybogames.com>
2019-07-29 12:31:47 +0200

    pickups

--- octopus.nix
+++ octopus.nix
@@ -10,15 +10,6 @@
         ./common/i3.nix
         ./common/node.nix
         ./common/golang.nix
-    ];
-
-    # docker and docker-compose
-    virtualisation.docker = {
-      enable = true;
-      extraOptions = "--log-level debug --debug";
-    };
-
-    environment.systemPackages = with pkgs; [
-      pkgs.docker_compose
+        ./common/docker.nix
     ];
 }
